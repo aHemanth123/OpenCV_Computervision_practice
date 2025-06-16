8252b3450e9f92d08ba867685f014883a39512b9
A . Hemanth Sai Manikanta <133625674+aHemanth123@users.noreply.github.com>
2025-06-16 15:34:26 +0530

    Update 11_blurring.py

--- OpenCV/11_blurring.py
+++ OpenCV/11_blurring.py
@@ -11,11 +11,17 @@ cv.imshow('Img',img)
 # blur is applied to middle   as a result of surrounding pixels 
 
 #averaging at middle  by surrounding pixels --> pixel intensity 
+# Applies a normalized box filter (i.e., takes the average of all pixels in the kernel).
+# Blurs the image uniformly.
+
 average = cv.blur(img, (3,3))
 cv.imshow('avgblur',average)
 
 # gauassian blur --> each surrounding pixel is given particular pixel value 
 # u will get less blur 
+# Applies a weighted average where centre pixels have higher influence.
+# Preserves edges better than plain average blur.
+
 gauss = cv.GaussianBlur(img, (3,3) ,0)
 cv.imshow("guass blur",gauss)
 
@@ -24,7 +30,7 @@ median = cv.medianBlur(img, 3) #integer - if 3  kernel size 3,3 only
 cv.imshow('Median blur',median)
 
 #bilateral blur  --> applies bluring but retains the edges  in img 
-# traditional blur -->
+
 bilateral = cv.bilateralFilter(img, 10, 35 , 25 )  # explore the arg
 cv.imshow('BILateral',bilateral)
 #sigma space -> is nothing but how far distance it  will affect the img
