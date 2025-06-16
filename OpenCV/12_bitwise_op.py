6679d5be92fc31611de2885c8063630e0a9ac14a
A . Hemanth Sai Manikanta <133625674+aHemanth123@users.noreply.github.com>
2025-06-16 15:43:54 +0530

    Update 12_bitwise_op.py

--- OpenCV/12_bitwise_op.py
+++ OpenCV/12_bitwise_op.py
@@ -9,20 +9,21 @@ circle = cv.circle(blank.copy(), (200,200), 200,255,-1 )
 cv.imshow('Rectangle',rectangle)
 cv.imshow('circle',circle)
 
-#bitwise and 
+#bitwise and  --> intersection
 bitwise_and =  cv.bitwise_and(rectangle ,circle)
 cv.imshow('Bitwise And',bitwise_and)
 
-#bitwise or 
+#bitwise or   --> union
 bitwise_or  = cv.bitwise_or(rectangle,circle)
 cv.imshow('Bitwise or',bitwise_or)
 
-# bitwise_xor  -> non intersecting regions 
+# bitwise_xor  -> non-intersecting regions 
 bitwise_xor = cv.bitwise_xor(rectangle,circle)
 cv.imshow('xor',bitwise_xor)
 
-#  bit xor - or  -> bit and 
-# bit and - or ->  bit xor 
+# equation
+#  bit xor - bitwise_or  =  bit and 
+# bit and - bitwise_or  =   bit xor 
 
 # bit wise not   ---> inverts binary col
 bitwise_not = cv.bitwise_not(rectangle)
