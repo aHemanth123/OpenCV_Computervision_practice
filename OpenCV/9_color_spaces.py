34d1fb5c1d15391270e485b2f2e61a506635d47c
A . Hemanth Sai Manikanta <133625674+aHemanth123@users.noreply.github.com>
2025-06-16 14:55:02 +0530

    Update 9_color_spaces.py

--- OpenCV/9_color_spaces.py
+++ OpenCV/9_color_spaces.py
@@ -35,3 +35,35 @@ hsv_bgr  = cv.cvtColor(hsv,cv.COLOR_HSV2BGR)
 lab_bgr= cv.cvtColor(lab,cv.COLOR_Lab2BGR)
 cv.imshow('Lab--> bgr',lab_bgr)
 cv.waitKey(0)
+
+
+
+# Direct 
+
+| From → To       | OpenCV Code          |
+| --------------- | -------------------- |
+| BGR → RGB       | `cv.COLOR_BGR2RGB`   |
+| BGR → Grayscale | `cv.COLOR_BGR2GRAY`  |
+| BGR → HSV       | `cv.COLOR_BGR2HSV`   |
+| BGR → LAB       | `cv.COLOR_BGR2LAB`   |
+| BGR → YCrCb     | `cv.COLOR_BGR2YCrCb` |
+| RGB → BGR       | `cv.COLOR_RGB2BGR`   |
+| RGB → Grayscale | `cv.COLOR_RGB2GRAY`  |
+| RGB → HSV       | `cv.COLOR_RGB2HSV`   |
+| HSV → BGR       | `cv.COLOR_HSV2BGR`   |
+| LAB → BGR       | `cv.COLOR_LAB2BGR`   |
+| YCrCb → BGR     | `cv.COLOR_YCrCb2BGR` |
+| Grayscale → BGR | `cv.COLOR_GRAY2BGR`  |
+| Grayscale → RGB | `cv.COLOR_GRAY2RGB`  |
+
+
+
+#indirect 
+| Invalid Conversion | Why it Fails / Fix                | What To Do Instead |
+| ------------------ | --------------------------------- | ------------------ |
+| Grayscale → HSV    | Missing hue/saturation info       | `GRAY → BGR → HSV` |
+| Grayscale → LAB    | Missing color info                | `GRAY → BGR → LAB` |
+| HSV → Grayscale    | Hue/saturation can’t be collapsed | `HSV → BGR → GRAY` |
+| LAB → Grayscale    | Same reason                       | `LAB → BGR → GRAY` |
+| HSV → LAB          | Not directly convertible          | `HSV → BGR → LAB`  |
+| LAB → HSV          | Not directly convertible          | `LAB → BGR → HSV`  |
